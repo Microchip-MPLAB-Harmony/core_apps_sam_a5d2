5fc93e59ac5c816634032da86022b88f477c6d23
Syam Nair <syam.nair@microchip.com>
2021-02-02 14:12:38 +0530

    [DRV_MEMORY] Regenerated demo for driver_memory_sync_mx25l_at25df_read_write_sam_a5d2_xult_freertos
    MH3-50142
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/memory/sync/mx25l_at25df_read_write/firmware/src/config/sam_a5d2_xult_freertos/initialization.c
+++ apps/driver/memory/sync/mx25l_at25df_read_write/firmware/src/config/sam_a5d2_xult_freertos/initialization.c
@@ -245,6 +245,7 @@ const SYS_TIME_INIT sysTimeInitData =
 
 void SYS_Initialize ( void* data )
 {
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -256,7 +257,7 @@ void SYS_Initialize ( void* data )
     MMU_Initialize();
     Matrix_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
