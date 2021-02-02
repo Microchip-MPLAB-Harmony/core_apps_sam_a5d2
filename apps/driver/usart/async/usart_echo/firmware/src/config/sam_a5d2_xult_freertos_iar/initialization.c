d6cfc5b8f3fc5c566e57b07a621dfc62f77b7758
Syam Nair <syam.nair@microchip.com>
2021-02-02 14:48:54 +0530

    [DRV_USART] Regenerated demo for driver_usart_async_usart_echo_sam_a5d2_xult_freertos_iar
    MH3-50298
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/usart/async/usart_echo/firmware/src/config/sam_a5d2_xult_freertos_iar/initialization.c
+++ apps/driver/usart/async/usart_echo/firmware/src/config/sam_a5d2_xult_freertos_iar/initialization.c
@@ -74,7 +74,7 @@ const DRV_USART_PLIB_INTERFACE drvUsart0PlibAPI = {
     .read = (DRV_USART_PLIB_READ)UART1_Read,
     .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)UART1_ReadIsBusy,
     .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)UART1_ReadCountGet,
-	.readAbort = (DRV_USART_PLIB_READ_ABORT)UART1_ReadAbort,
+    .readAbort = (DRV_USART_PLIB_READ_ABORT)UART1_ReadAbort,
     .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)UART1_WriteCallbackRegister,
     .write = (DRV_USART_PLIB_WRITE)UART1_Write,
     .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)UART1_WriteIsBusy,
@@ -126,6 +126,8 @@ const DRV_USART_INIT drvUsart0InitData =
     .remapStopBits = drvUsart0remapStopBits,
 
     .remapError = drvUsart0remapError,
+
+    .dataWidth = DRV_USART_DATA_8_BIT,
 };
 
 // </editor-fold>
@@ -174,6 +176,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -190,7 +193,7 @@ void SYS_Initialize ( void* data )
 
     PLIB_L2CC_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
