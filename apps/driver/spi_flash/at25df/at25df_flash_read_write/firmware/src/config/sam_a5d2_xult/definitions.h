a5f27f15e41facd779fb21b1074ee562c263dd49
Syam Nair <syam.nair@microchip.com>
2021-02-02 14:41:34 +0530

    [DRV_SPI_FLASH] Regenerated demo for driver_spi_flash_at25df_at25df_flash_read_write_sam_a5d2_xult
    MH3-50291
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/spi_flash/at25df/at25df_flash_read_write/firmware/src/config/sam_a5d2_xult/definitions.h
+++ apps/driver/spi_flash/at25df/at25df_flash_read_write/firmware/src/config/sam_a5d2_xult/definitions.h
@@ -55,7 +55,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
-#include "peripheral/spi/plib_spi0.h"
+#include "peripheral/spi/spi_master/plib_spi0_master.h"
 #include "driver/spi_flash/at25df/drv_at25df.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
@@ -73,6 +73,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 498000000
 
 // *****************************************************************************
 // *****************************************************************************
@@ -166,22 +169,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* AT25DF Driver Object */
--- apps/driver/spi_flash/at25df/at25df_flash_read_write/firmware/src/config/sam_a5d2_xult/initialization.c
+++ apps/driver/spi_flash/at25df/at25df_flash_read_write/firmware/src/config/sam_a5d2_xult/initialization.c
@@ -146,6 +146,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -158,7 +159,7 @@ void SYS_Initialize ( void* data )
     MMU_Initialize();
     Matrix_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
