7b74f3b52a285510b74142a061beacc030aee3eb
Syam Nair <syam.nair@microchip.com>
2021-02-02 15:17:43 +0530

    [SYS_FS] Regenerated demo for fs_spi_flash_fat_sam_a5d2_xult_iar
    MH3-50322
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/fs/spi_flash_fat/firmware/src/config/sam_a5d2_xult_iar/initialization.c
+++ apps/fs/spi_flash_fat/firmware/src/config/sam_a5d2_xult_iar/initialization.c
@@ -238,6 +238,7 @@ const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 
 void SYS_Initialize ( void* data )
 {
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -250,7 +251,7 @@ void SYS_Initialize ( void* data )
 
     PLIB_L2CC_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
