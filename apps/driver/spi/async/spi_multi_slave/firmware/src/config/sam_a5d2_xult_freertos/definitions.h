199d4454239b8def972a1d3bda1d65c9e67e5e99
Syam Nair <syam.nair@microchip.com>
2021-02-02 14:23:44 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_multi_slave_sam_a5d2_xult_freertos
    MH3-50153
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_a5d2_xult_freertos/definitions.h
+++ apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_a5d2_xult_freertos/definitions.h
@@ -56,7 +56,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
-#include "peripheral/spi/plib_spi1.h"
+#include "peripheral/spi/spi_master/plib_spi1_master.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "driver/spi/drv_spi.h"
@@ -79,6 +79,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 498000000
 
 // *****************************************************************************
 // *****************************************************************************
@@ -172,22 +175,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* SPI0 Driver Object */
--- apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_a5d2_xult_freertos/initialization.c
+++ apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_a5d2_xult_freertos/initialization.c
@@ -179,6 +179,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -193,7 +194,7 @@ void SYS_Initialize ( void* data )
 
     PLIB_L2CC_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
