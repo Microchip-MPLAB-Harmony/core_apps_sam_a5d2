1386ab7780046fbac0cb838000e4e443b9e1d4b1
Syam Nair <syam.nair@microchip.com>
2021-02-02 14:03:12 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_sam_a5d2_xult_freertos
    MH3-50132
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_a5d2_xult_freertos/initialization.c
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_a5d2_xult_freertos/initialization.c
@@ -154,6 +154,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -168,7 +169,7 @@ void SYS_Initialize ( void* data )
 
     PLIB_L2CC_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
