0e13b7f0dc8b69e336289d71856e71cc62a6db45
Syam Nair <syam.nair@microchip.com>
2021-02-02 15:20:26 +0530

    [SYS_FS] Regenerated demo for fs_sqi_flash_fat_sam_a5d2_xult_freertos
    MH3-50324
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/fs/sqi_flash_fat/firmware/src/config/sam_a5d2_xult_freertos/initialization.c
+++ apps/fs/sqi_flash_fat/firmware/src/config/sam_a5d2_xult_freertos/initialization.c
@@ -232,6 +232,7 @@ const SYS_TIME_INIT sysTimeInitData =
 
 void SYS_Initialize ( void* data )
 {
+
   
     CLK_Initialize();
 	PIO_Initialize();
@@ -243,7 +244,7 @@ void SYS_Initialize ( void* data )
     MMU_Initialize();
     Matrix_Initialize();
 
-    INT_Initialize();
+    AIC_INT_Initialize();
     
 	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
 
