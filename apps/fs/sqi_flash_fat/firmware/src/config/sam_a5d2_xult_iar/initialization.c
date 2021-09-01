4e4235753f4fe8aa08f2a8553e8c2f9a202eb347
Syam Nair <syam.nair@microchip.com>
2021-09-01 14:49:07 +0530

    [SYS_FS] Regenerated demo for fs_sqi_flash_fat_sam_a5d2_xult_iar
    MH3-56163
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/fs/sqi_flash_fat/firmware/src/config/sam_a5d2_xult_iar/initialization.c
+++ apps/fs/sqi_flash_fat/firmware/src/config/sam_a5d2_xult_iar/initialization.c
@@ -175,13 +175,15 @@ const SYS_FS_FUNCTIONS FatFsFunctions =
 };
 
 
+
 const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 {
     {
         .nativeFileSystemType = FAT,
         .nativeFileSystemFunctions = &FatFsFunctions
-    }
+    },
 };
+
 
 // </editor-fold>
 
@@ -217,6 +219,7 @@ void SYS_Initialize ( void* data )
 {
 
   
+    MMU_Initialize();
     CLK_Initialize();
 	PIO_Initialize();
 
@@ -225,7 +228,6 @@ void SYS_Initialize ( void* data )
     QSPI0_Initialize();
 
 	BSP_Initialize();
-    MMU_Initialize();
     Matrix_Initialize();
 
     PLIB_L2CC_Initialize();
