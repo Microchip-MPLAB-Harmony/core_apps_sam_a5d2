df80df2ff4ce3c6223c580bb8fdba33c5d0277c3
Syam Nair <syam.nair@microchip.com>
2021-09-01 14:04:30 +0530

    [DRV_SQI_FLASH] Regenerated demo for driver_sqi_flash_mx25l_mx25l_flash_read_write_sam_a5d2_xult_iar
    MH3-56131
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/sqi_flash/mx25l/mx25l_flash_read_write/firmware/src/config/sam_a5d2_xult_iar/initialization.c
+++ apps/driver/sqi_flash/mx25l/mx25l_flash_read_write/firmware/src/config/sam_a5d2_xult_iar/initialization.c
@@ -125,6 +125,7 @@ void SYS_Initialize ( void* data )
 {
 
   
+    MMU_Initialize();
     CLK_Initialize();
 	PIO_Initialize();
 
@@ -135,7 +136,6 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
 	PIT_TimerInitialize();
 
-    MMU_Initialize();
     Matrix_Initialize();
 
     PLIB_L2CC_Initialize();
