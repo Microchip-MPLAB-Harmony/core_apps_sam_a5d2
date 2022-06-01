ad5914cbd7a27f74f8a8972c4b1b5706ad52e88b
Syam Nair <syam.nair@microchip.com>
2022-06-02 02:10:25 +0530

    [SYS_FS] Regenerated demo for fs_sdmmc_fat_sam_a5d27_som1_ek_freertos
    MH3-65054
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/fs/sdmmc_fat/firmware/src/config/sam_a5d27_som1_ek_freertos/peripheral/clk/plib_clk.c
+++ apps/fs/sdmmc_fat/firmware/src/config/sam_a5d27_som1_ek_freertos/peripheral/clk/plib_clk.c
@@ -71,9 +71,9 @@ static void CLK_PeripheralClockInitialize(void)
     /* Enable clock for the selected peripherals, since the rom boot will turn on
      * certain clocks turn off all clocks not expressly enabled */
    	PMC_REGS->PMC_PCER0=0x80042000;
-    PMC_REGS->PMC_PCDR0=~0x80042000;
+    PMC_REGS->PMC_PCDR0=~0x80042000U;
     PMC_REGS->PMC_PCER1=0x8;
-    PMC_REGS->PMC_PCDR1=~0x8;
+    PMC_REGS->PMC_PCDR1=~0x8U;
 }
 
 
