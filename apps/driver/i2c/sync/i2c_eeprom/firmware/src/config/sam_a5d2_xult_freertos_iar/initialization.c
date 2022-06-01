0374176a6d6688bf1efea24f01e12f5f8ae6e595
Syam Nair <syam.nair@microchip.com>
2022-06-02 00:39:45 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_sam_a5d2_xult_freertos_iar
    MH3-64999
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_a5d2_xult_freertos_iar/initialization.c
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_a5d2_xult_freertos_iar/initialization.c
@@ -80,6 +80,9 @@ const DRV_I2C_PLIB_INTERFACE drvI2C0PLibAPI = {
     /* I2C PLib Transfer Write Read Add function */
     .writeRead = (DRV_I2C_PLIB_WRITE_READ)TWIHS1_WriteRead,
 
+    /*I2C PLib Tranfer Abort function */
+    .transferAbort = (DRV_I2C_PLIB_TRANSFER_ABORT)TWIHS1_TransferAbort,
+
     /* I2C PLib Transfer Status function */
     .errorGet = (DRV_I2C_PLIB_ERROR_GET)TWIHS1_ErrorGet,
 
@@ -158,7 +161,8 @@ void SYS_Initialize ( void* data )
   
     MMU_Initialize();
     CLK_Initialize();
-	PIO_Initialize();
+    PIO_Initialize();
+
 
 
 
