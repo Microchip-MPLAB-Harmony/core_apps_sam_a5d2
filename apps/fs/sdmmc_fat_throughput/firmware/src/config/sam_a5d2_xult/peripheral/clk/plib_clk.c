292404422302856647c5755fd46c17a772f84cee
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-12 16:21:02 +0530

    [SYS_FS] Regenerated demo for fs_sdmmc_fat_throughput_sam_a5d2_xult
    MH3-76461

--- apps/fs/sdmmc_fat_throughput/firmware/src/config/sam_a5d2_xult/peripheral/clk/plib_clk.c
+++ apps/fs/sdmmc_fat_throughput/firmware/src/config/sam_a5d2_xult/peripheral/clk/plib_clk.c
@@ -70,9 +70,9 @@ static void CLK_PeripheralClockInitialize(void)
 {
     /* Enable clock for the selected peripherals, since the rom boot will turn on
      * certain clocks turn off all clocks not expressly enabled */
-   	PMC_REGS->PMC_PCER0=0x2042000;
+    PMC_REGS->PMC_PCER0=0x2042000U;
     PMC_REGS->PMC_PCDR0=~0x2042000U;
-    PMC_REGS->PMC_PCER1=0x9;
+    PMC_REGS->PMC_PCER1=0x9U;
     PMC_REGS->PMC_PCDR1=~0x9U;
 }
 
@@ -87,11 +87,11 @@ void CLK_Initialize( void )
     /* Initialize Audio PLL */
     CLK_AudioPLLInitialize();
 
-	/* Initialize Generic Clock */
-	CLK_GenericClockInitialize();
+    /* Initialize Generic Clock */
+    CLK_GenericClockInitialize();
 
-	/* Initialize Peripheral Clock */
-	CLK_PeripheralClockInitialize();
+    /* Initialize Peripheral Clock */
+    CLK_PeripheralClockInitialize();
 
 }
 
