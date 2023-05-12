4be0134ad550cfc0ccf29eb2b4f9c685cdab24d0
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-12 16:14:25 +0530

    [DRV_SPI] Regenerated demo for driver_spi_sync_spi_multi_slave_sam_a5d2_xult_freertos
    MH3-76442

--- apps/driver/spi/sync/spi_multi_slave/firmware/src/config/sam_a5d2_xult_freertos/interrupts.h
+++ apps/driver/spi/sync/spi_multi_slave/firmware/src/config/sam_a5d2_xult_freertos/interrupts.h
@@ -57,6 +57,13 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void FreeRTOS_Tick_Handler (void);
+void XDMAC0_InterruptHandler (void);
+void SPI1_InterruptHandler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
