c405aed0a25f93780bb7422a10eb99bf359a8e6c
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-12 16:14:33 +0530

    [SYS_FS] Regenerated demo for fs_sdmmc_fat_sam_a5d2_xult_freertos
    MH3-76460

--- apps/fs/sdmmc_fat/firmware/src/config/sam_a5d2_xult_freertos/interrupts.h
+++ apps/fs/sdmmc_fat/firmware/src/config/sam_a5d2_xult_freertos/interrupts.h
@@ -57,6 +57,13 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void FreeRTOS_Tick_Handler (void);
+void SDMMC1_InterruptHandler (void);
+void TC0_InterruptHandler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
