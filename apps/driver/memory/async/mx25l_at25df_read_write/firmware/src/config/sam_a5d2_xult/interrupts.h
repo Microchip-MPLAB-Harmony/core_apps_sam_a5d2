3361e600567c74261ca9dd027a5949a9962f3cf7
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-12 16:20:47 +0530

    [DRV_MEMORY] Regenerated demo for driver_memory_async_mx25l_at25df_read_write_sam_a5d2_xult
    MH3-76431

--- apps/driver/memory/async/mx25l_at25df_read_write/firmware/src/config/sam_a5d2_xult/interrupts.h
+++ apps/driver/memory/async/mx25l_at25df_read_write/firmware/src/config/sam_a5d2_xult/interrupts.h
@@ -57,6 +57,13 @@
 // *****************************************************************************
 // *****************************************************************************
 
+void PIT_InterruptHandler (void);
+void SPI0_InterruptHandler (void);
+void TC0_InterruptHandler (void);
+
+
+/* Interrupt Handler for spurious interrupts */
+void SPURIOUS_INTERRUPT_Handler (void);
 
 
 #endif // INTERRUPTS_H
