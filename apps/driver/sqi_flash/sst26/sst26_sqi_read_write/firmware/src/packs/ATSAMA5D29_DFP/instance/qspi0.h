b28ff9bd6e5730792bad401a7f8afcd1615ca16c
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 09:30:10 +0000

    [APP] Regenerated demo for driver__sqi_flash__sst26__sst26_sqi_read_write__firmware
    MH3C-338

--- apps/driver/sqi_flash/sst26/sst26_sqi_read_write/firmware/src/packs/ATSAMA5D29_DFP/instance/qspi0.h
+++ apps/driver/sqi_flash/sst26/sst26_sqi_read_write/firmware/src/packs/ATSAMA5D29_DFP/instance/qspi0.h
@@ -1,7 +1,9 @@
 /*
  * Instance header file for ATSAMA5D29TA100
  *
- * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,15 +19,15 @@
  *
  */
 
-/* file generated from device description file (ATDF) version 2024-06-24T14:43:55Z */
+/* file generated from device description file (ATDF) version 2025-07-09T11:00:00Z */
 #ifndef _SAMA5D2_QSPI0_INSTANCE_
 #define _SAMA5D2_QSPI0_INSTANCE_
 
 
 /* ========== Instance Parameter definitions for QSPI0 peripheral ========== */
-#define QSPI0_CLOCK_ID                           (52)       
-#define QSPI0_DMAC_ID_RX                         (5)        
-#define QSPI0_DMAC_ID_TX                         (4)        
-#define QSPI0_INSTANCE_ID                        (52)       
+#define QSPI0_CLOCK_ID                           (52)
+#define QSPI0_DMAC_ID_RX                         (5)
+#define QSPI0_DMAC_ID_TX                         (4)
+#define QSPI0_INSTANCE_ID                        (52)
 
 #endif /* _SAMA5D2_QSPI0_INSTANCE_ */
