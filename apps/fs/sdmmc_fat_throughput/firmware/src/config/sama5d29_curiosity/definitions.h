ca044c21ca4cbd01e5723180c0d3f81bb885c35e
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 09:29:30 +0000

    [APP] Regenerated demo for fs__sdmmc_fat_throughput__firmware
    MH3C-338

--- apps/fs/sdmmc_fat_throughput/firmware/src/config/sama5d29_curiosity/definitions.h
+++ apps/fs/sdmmc_fat_throughput/firmware/src/config/sama5d29_curiosity/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -49,8 +49,8 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include "bsp/bsp.h"
 #include "driver/sdmmc/drv_sdmmc.h"
+#include "bsp/bsp.h"
 #include "peripheral/mmu/plib_mmu.h"
 #include "peripheral/matrix/plib_matrix.h"
 #include "peripheral/clk/plib_clk.h"
--- apps/fs/sdmmc_fat_throughput/firmware/src/packs/ATSAMA5D29_DFP/component/sckc.h
+++ apps/fs/sdmmc_fat_throughput/firmware/src/packs/ATSAMA5D29_DFP/component/sckc.h
@@ -1,7 +1,9 @@
 /*
  * Component description for SCKC
  *
- * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ * Copyright (c) 2025 Microchip Technology Inc. and its subsidiaries.
+ *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,33 +19,35 @@
  *
  */
 
-/* file generated from device description file (ATDF) version 2024-06-24T14:43:55Z */
+/*  file generated from device description file (ATDF) version 2025-07-09T11:00:00Z  */
 #ifndef _SAMA5D2_SCKC_COMPONENT_H_
 #define _SAMA5D2_SCKC_COMPONENT_H_
 
+#include "core_ca.h"
+
 /* ************************************************************************** */
-/*   SOFTWARE API DEFINITION FOR SCKC                                         */
+/*                      SOFTWARE API DEFINITION FOR SCKC                      */
 /* ************************************************************************** */
 
 /* -------- SCKC_CR : (SCKC Offset: 0x00) (R/W 32) Slow Clock Controller Configuration Register -------- */
 #define SCKC_CR_OSCSEL_Pos                    _UINT32_(3)                                          /* (SCKC_CR) Slow Clock Selector Position */
 #define SCKC_CR_OSCSEL_Msk                    (_UINT32_(0x1) << SCKC_CR_OSCSEL_Pos)                /* (SCKC_CR) Slow Clock Selector Mask */
-#define SCKC_CR_OSCSEL(value)                 (SCKC_CR_OSCSEL_Msk & (_UINT32_(value) << SCKC_CR_OSCSEL_Pos)) /* Assigment of value for OSCSEL in the SCKC_CR register */
+#define SCKC_CR_OSCSEL(value)                 (SCKC_CR_OSCSEL_Msk & (_UINT32_(value) << SCKC_CR_OSCSEL_Pos)) /* Assignment of value for OSCSEL in the SCKC_CR register */
 #define   SCKC_CR_OSCSEL_RC_Val               _UINT32_(0x0)                                        /* (SCKC_CR) Slow clock is the embedded 64 kHz (typical) RC oscillator.  */
 #define   SCKC_CR_OSCSEL_XTAL_Val             _UINT32_(0x1)                                        /* (SCKC_CR) Slow clock is the 32.768 kHz crystal oscillator.  */
-#define SCKC_CR_OSCSEL_RC                     (SCKC_CR_OSCSEL_RC_Val << SCKC_CR_OSCSEL_Pos)        /* (SCKC_CR) Slow clock is the embedded 64 kHz (typical) RC oscillator. Position  */
-#define SCKC_CR_OSCSEL_XTAL                   (SCKC_CR_OSCSEL_XTAL_Val << SCKC_CR_OSCSEL_Pos)      /* (SCKC_CR) Slow clock is the 32.768 kHz crystal oscillator. Position  */
+#define SCKC_CR_OSCSEL_RC                     (SCKC_CR_OSCSEL_RC_Val << SCKC_CR_OSCSEL_Pos)        /* (SCKC_CR) Slow clock is the embedded 64 kHz (typical) RC oscillator. Position */
+#define SCKC_CR_OSCSEL_XTAL                   (SCKC_CR_OSCSEL_XTAL_Val << SCKC_CR_OSCSEL_Pos)      /* (SCKC_CR) Slow clock is the 32.768 kHz crystal oscillator. Position */
 #define SCKC_CR_Msk                           _UINT32_(0x00000008)                                 /* (SCKC_CR) Register Mask  */
 
 
-/** \brief SCKC register offsets definitions */
+/* SCKC register offsets definitions */
 #define SCKC_CR_REG_OFST               _UINT32_(0x00)      /* (SCKC_CR) Slow Clock Controller Configuration Register Offset */
 
 #if !(defined(__ASSEMBLER__) || defined(__IAR_SYSTEMS_ASM__))
-/** \brief SCKC register API structure */
+/* SCKC register API structure */
 typedef struct
 {
-  __IO  uint32_t                       SCKC_CR;            /**< Offset: 0x00 (R/W  32) Slow Clock Controller Configuration Register */
+  __IO  uint32_t                       SCKC_CR;            /* Offset: 0x00 (R/W  32) Slow Clock Controller Configuration Register */
 } sckc_registers_t;
 
 
